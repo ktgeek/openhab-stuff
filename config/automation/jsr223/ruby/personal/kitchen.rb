06f66921fcac1709879f1d7f3a9274b07ef19f2b
Keith T. Garner <kgarner@kgarner.com>
2021-09-19 14:41:06 -0500

    fix logic error and simplify kitchen

--- config/automation/jsr223/ruby/personal/kitchen.rb
+++ config/automation/jsr223/ruby/personal/kitchen.rb
@@ -12,18 +12,13 @@ def reset_basement
   basement_hiome_room_ids.each do |i|
     HTTP::sendHttpPutRequest("http://hiome.kgarner.com/api/1/rooms/#{i}", "application/json", '{"occupancy_count": 0}')
   end
-  C_All_Lights.members.each { |i| i.off if i.off? }
+  C_All_Lights.members.each { |i| i.off unless i.off? }
 end
 
 rule "when the kitchen accent switchs have a scene change" do
   updated FF_Kitchen_Accents_Scene.members
 
-  run do |event|
-    case(event.state)
-    when Homeseer::PADDLE_DOWN_TWO_CLICKS
-      reset_basement
-    end
-  end
+  run { |event| reset_basement if event.state == Homeseer::PADDLE_DOWN_TWO_CLICKS }
 end
 
 rule "when ktich lights scene change" do
