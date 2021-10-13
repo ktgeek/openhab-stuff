fca935f803845bb7eaf980b002b0b1a22e7ee166
Keith T. Garner <kgarner@kgarner.com>
2021-10-13 15:50:23 -0500

    rewrite zoom automations to be better (i hope)

--- config/automation/jsr223/ruby/personal/office.rb
+++ config/automation/jsr223/ruby/personal/office.rb
@@ -2,43 +2,43 @@
 
 require 'openhab'
 require 'wifi_led'
+require 'utilities'
 
 stored_led_states = nil
 
-rule "when a zoom meeting is happening" do
-  changed Zoom_Active_Switch
+rule "when a zoom meeting is on" do
+  changed Zoom_Active_Switch, to: ON
 
-  run do |event|
-    commands = 0
+  run do
+    stored_led_states = store_states Office_Door_LED_Program if Office_Door_LED_Power.on? && Office_Door_LED_Program != WifiLED::Program::NO_PROGRAM
 
-    if event.state == ON
-      if Office_Door_LED_Power.on? && Office_Door_LED_Program != WifiLED::Program::NO_PROGRAM
-        stored_led_states = store_states Office_Door_LED_Program
-      end
+    commands = []
 
-      if Office_Door_LED_Power.off?
-        Office_Door_LED_Power.on
-        commands += 1
-      end
+    commands << [Office_Door_LED_Power, ON] if Office_Door_LED_Power.off?
 
-      # The wifi LED controller seems to get really angry if I send too many wifi commands at the same time,
-      # so we'll add a delay of 2 seconds per number of commands run
-      unless Office_Door_LED_Program == WifiLED::Program::NO_PROGRAM
-        after((commands*2).seconds) { Office_Door_LED_Program << WifiLED::Program::NO_PROGRAM }
-        commands += 1
-      end
+    commands << [Office_Door_LED_Program, WifiLED::Program::NO_PROGRAM] unless Office_Door_LED_Program == WifiLED::Program::NO_PROGRAM
 
-      current_color = Office_Door_LED_Color.state
-      if [current_color.hue, current_color.saturation, current_color.brightness].join(',') != WifiLED::Color::RED
-        after((commands*2).seconds) { Office_Door_LED_Color << WifiLED::Color::RED }
-      end
-    else
-      if stored_led_states
+    commands << [Office_Door_LED_Color, WifiLED::Color::RED] if Utilities::color_to_string(Office_Door_LED_Color) != WifiLED::Color::RED
+
+    # The wifi LED controller seems to get really angry if I send too many wifi commands at the same time,
+    # so we'll add a delay of 2 seconds per number of commands run
+    last_command = commands.last
+    commands.each do |c|
+      c[0] << c[1]
+      sleep 2 unless c == last_command
+    end
+  end
+end
+
+rule "when a zoom meeting is over" do
+  changed Zoom_Active_Switch, to: OFF
+
+  run do
+    if stored_led_states
         stored_led_states&.restore_changes
         stored_led_states = nil
-      else
-        Office_Door_LED_Power.off unless Office_Door_LED_Power.off?
-      end
+    else
+      Office_Door_LED_Power.off unless Office_Door_LED_Power.off?
     end
   end
 end
