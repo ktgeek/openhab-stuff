caea1a268d8f786833390bef82978c8e815ccde0
Keith T. Garner <kgarner@kgarner.com>
2023-11-10 13:45:44 -0600

    make the zwavejs profile more generic

--- config/automation/ruby/profiles.rb
+++ config/automation/ruby/profiles.rb
@@ -2,10 +2,12 @@
 
 require 'json'
 
-profile(:zwavejs_scene_handler) do |event, callback:, state:|
+profile(:zwavejs_int_handler) do |event, callback:, state:, configuration:|
   next true unless event == :state_from_handler
 
-  result = JSON.parse(state).dig("value")&.to_i
+  path = configuration.fetch("path", "value")
+
+  result = JSON.parse(state).dig(*path.split("."))&.to_i
 
   callback.send_update(result)
 
