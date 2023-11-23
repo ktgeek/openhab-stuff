e57445b7dc16f47fb5a684a408108affc761270c
Keith T. Garner <kgarner@kgarner.com>
2023-11-23 09:22:03 -0600

    remove cube and rules, add remote rules

--- config/automation/ruby/bedroom.rb
+++ config/automation/ruby/bedroom.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "aqara_cube"
-
 updated Bedroom_Ceiling_Fan_Speed do |event|
   next unless (0..3).cover?(event.state)
 
@@ -12,18 +10,19 @@ received_command Bedroom_Ceiling_Fan_Power, command: OFF do
   Bedroom_Ceiling_Fan_Speed << 0
 end
 
-updated Bedroom_Cube_Action, to: AqaraCube::Action::TAP do
+updated Bedroom_Remotes_Action.members, to: %w[1_single 2_single 3_single] do |event|
+  Bedroom_Ceiling_Fan_Speed << event.state[0].to_i
+end
+
+updated Bedroom_Remotes_Action.members, to: %w[1_double 2_double 3_double] do
+  Bedroom_Ceiling_Fan_Speed << 0
+end
+
+updated Bedroom_Remotes_Action.members, to: "4_single" do |event|
+  switch = items["Bedroom_#{event.item_name.split('_')[1]}_Light_Switch"]
+  switch.toggle
+end
+
+updated Bedroom_Remotes_Action.members, to: "4_double" do
   Bedroom_Ceiling_Fan_Light_Power.toggle
-end
-
-updated Bedroom_Cube_Action, to: AqaraCube::Action::ROTATE_RIGHT do
-  Bedroom_Table_Light_Switch.on
-end
-
-updated Bedroom_Cube_Action, to: AqaraCube::Action::ROTATE_LEFT do
-  Bedroom_Table_Light_Switch.off
-end
-
-updated Bedroom_Cube_Action, to: [AqaraCube::Action::FLIP90, AqaraCube::Action::FLIP180] do
-  Bedroom_Ceiling_Fan_Speed << Bedroom_Cube_Side.state if Bedroom_Cube_Side.state < 4
 end
