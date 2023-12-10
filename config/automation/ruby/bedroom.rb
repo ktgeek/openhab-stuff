fa21d84502bce647fbab554acc6e938754076578
Keith T. Garner <kgarner@kgarner.com>
2023-12-10 12:15:42 -0600

    tersify rules

--- config/automation/ruby/bedroom.rb
+++ config/automation/ruby/bedroom.rb
@@ -6,23 +6,17 @@ updated Bedroom_Ceiling_Fan_Speed do |event|
   Bedroom_Ceiling_Fan_Power.update(event.state.positive?)
 end
 
-received_command Bedroom_Ceiling_Fan_Power, command: OFF do
-  Bedroom_Ceiling_Fan_Speed << 0
-end
+received_command(Bedroom_Ceiling_Fan_Power, command: OFF) { Bedroom_Ceiling_Fan_Speed << 0 }
 
 updated Bedroom_Remotes_Action.members, to: %w[1_single 2_single 3_single] do |event|
   Bedroom_Ceiling_Fan_Speed << event.state[0].to_i
 end
 
-updated Bedroom_Remotes_Action.members, to: %w[1_double 2_double 3_double] do
-  Bedroom_Ceiling_Fan_Speed << 0
-end
+updated(Bedroom_Remotes_Action.members, to: %w[1_double 2_double 3_double]) { Bedroom_Ceiling_Fan_Speed << 0 }
 
 updated Bedroom_Remotes_Action.members, to: "4_single" do |event|
   switch = items["Bedroom_#{event.item_name.split('_')[1]}_Light_Switch"]
   switch.toggle
 end
 
-updated Bedroom_Remotes_Action.members, to: "4_double" do
-  Bedroom_Ceiling_Fan_Light_Power.toggle
-end
+updated(Bedroom_Remotes_Action.members, to: "4_double") { Bedroom_Ceiling_Fan_Light_Power.toggle }
