6eb6914bef76feed6db7c02dd98106de74466894
Keith T. Garner <kgarner@kgarner.com>
2023-12-27 15:09:44 -0600

    switch to changed for bedroom remotes

--- config/automation/ruby/bedroom.rb
+++ config/automation/ruby/bedroom.rb
@@ -8,23 +8,28 @@ end
 
 received_command(Bedroom_Ceiling_Fan_Power, command: OFF) { Bedroom_Ceiling_Fan_Speed << 0 }
 
-updated Bedroom_Remotes_Action.members, to: %w[1_single 2_single 3_single] do |event|
+changed Bedroom_Remotes_Action.members, to: %w[1_single 2_single 3_single] do |event|
   Bedroom_Ceiling_Fan_Speed << event.state[0].to_i
 end
 
-updated(Bedroom_Remotes_Action.members, to: %w[1_double 2_double 3_double]) { Bedroom_Ceiling_Fan_Speed << 0 }
+changed(Bedroom_Remotes_Action.members, to: %w[1_double 2_double 3_double]) { Bedroom_Ceiling_Fan_Speed << 0 }
 
-updated Bedroom_Remotes_Action.members, to: "4_single" do |event|
+changed Bedroom_Remotes_Action.members, to: "4_single" do |event|
   switch = items["Bedroom_#{event.item_name.split('_')[1]}_Light_Switch"]
   switch.toggle
 end
 
-updated(Bedroom_Remotes_Action.members, to: "4_double") { Bedroom_Ceiling_Fan_Light_Power.toggle }
+changed(Bedroom_Remotes_Action.members, to: "4_double") { Bedroom_Ceiling_Fan_Light_Power.toggle }
 
 changed Bedroom_Keiths_Closet_Contact_Sensor do |event|
   Bedroom_Keith_Closet_Light.ensure << (event.state == OPEN)
 end
 
-changed Bedroom_Sarahs_Closet_Contact_Sensor do |event|
-  Bedroom_Sarah_Closet_Light.ensure << (event.state == OPEN)
+changed Bedroom_Sarahs_Closet_Contact_Sensor, to: OPEN do |event|
+  timers.cancel(event.item)
+  Bedroom_Sarah_Closet_Light.ensure.on
+end
+
+changed Bedroom_Sarahs_Closet_Contact_Sensor, to: CLOSED do |event|
+  after(30.seconds, id: event.item) { Bedroom_Sarah_Closet_Light.ensure.off }
 end
