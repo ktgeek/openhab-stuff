85eb4ed9388acb60cd44b8284957d340ae463b84
Keith T. Garner <kgarner@kgarner.com>
2024-01-04 15:38:58 -0600

    remove commented out rules we won't do anymore

--- config/automation/ruby/sun.rb
+++ config/automation/ruby/sun.rb
@@ -3,16 +3,6 @@
 # on_start is trigger for startup times
 # TODO: add rule to set Sun_Status at start up
 
-# rule "when daylight starts" do
-#   channel "astro:sun:local:daylight#event", triggered: "START"
-#   run { Sun_Status << "UP" }
-# end
-
-# rule "when daylight ends" do
-#   channel "astro:sun:local:daylight#event", triggered: "END"
-#   run { Sun_Status << "DOWN" }
-# end
-
 updated Entrance_Luminance do |event|
   Sun_Status.ensure << (event.state.to_i < 170 ? "DOWN" : "UP")
 end
