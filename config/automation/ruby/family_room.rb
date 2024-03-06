363ea7e3a1e736d90e7815636677c4aa3735dcbf
Keith T. Garner <kgarner@kgarner.com>
2024-03-06 11:37:48 -0600

    add zigbee remote for family room fan

--- config/automation/ruby/family_room.rb
+++ config/automation/ruby/family_room.rb
@@ -5,3 +5,15 @@ rule "when family room lights turn on/off, turn on/off LEDs" do
 
   run { |event| FamilyRoom_LED_Power.ensure << event.state }
 end
+
+changed Family_Room_Fan_Button_Action, to: "single" do
+  Family_Room_Ceiling_Fan_Light_Power.toggle
+end
+
+changed Family_Room_Fan_Button_Action, to: "double" do
+  FamilyRoom_Lights_Switch.toggle
+end
+
+changed Family_Room_Fan_Button_Action, to: "hold" do
+  Family_Room_Ceiling_Fan_Fan_Power.toggle
+end
