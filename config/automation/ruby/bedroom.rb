fcb8da5de183e660ceced0df39ab5713330070f4
Keith T. Garner <kgarner@kgarner.com>
2024-03-06 11:41:11 -0600

    add scene switch to bedroom

--- config/automation/ruby/bedroom.rb
+++ config/automation/ruby/bedroom.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'zwave'
+
 updated Bedroom_Ceiling_Fan_Speed do |event|
   next unless (0..3).cover?(event.state)
 
@@ -21,6 +23,8 @@ end
 
 changed(Bedroom_Remotes_Action.members, to: "4_double") { Bedroom_Ceiling_Fan_Light_Power.toggle }
 
+changed(Bedroom_Table_Remote_Action, to: "4_hold") { Bedroom_Sarah_Light_Switch.toggle }
+
 changed Bedroom_Keiths_Closet_Contact_Sensor do |event|
   Bedroom_Keith_Closet_Light.ensure << (event.state == OPEN)
 end
@@ -32,4 +36,29 @@ end
 
 changed Bedroom_Sarahs_Closet_Contact_Sensor, to: CLOSED do |event|
   after(30.seconds, id: event.item) { Bedroom_Sarah_Closet_Light.ensure.off }
+end
+
+# sarah right, keith left
+changed Bedroom_Scene_Switch_Scene_1, to: ZWave::PADDLE_CLICK do
+  Bedroom_Table_Light_Switch.toggle
+end
+
+changed Bedroom_Scene_Switch_Scene_2, to: ZWave::PADDLE_CLICK do
+  Bedroom_Sarah_Light_Switch.toggle
+end
+
+changed Bedroom_Scene_Switch_Scene_3, to: ZWave::PADDLE_CLICK do
+  if Bedroom_Ceiling_Fan_Power.on?
+    Bedroom_Ceiling_Fan_Speed.command(0)
+  else
+    Bedroom_Ceiling_Fan_Speed.command(3)
+  end
+end
+
+changed Bedroom_Scene_Switch_Scene_4, to: ZWave::PADDLE_CLICK do
+  Bedroom_Hallway_Light_Switch.toggle
+end
+
+changed Bedroom_Scene_Switch_Scene_5, to: ZWave::PADDLE_CLICK do
+  Bedroom_Ceiling_Fan_Light_Power.toggle
 end
