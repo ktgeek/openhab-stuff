df59db65fb6c166b8cc2193c129c16ff4657c59b
Keith T. Garner <kgarner@kgarner.com>
2024-03-06 11:58:52 -0600

    side yard light is now a smart bulb

--- config/automation/ruby/laundry_room.rb
+++ config/automation/ruby/laundry_room.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+require "zwave"
+
 rule "when the laundry room door opens" do
   changed Laundry_Room_Door_Contact, to: OPEN
 
-  run { Side_Yard_Lights_Switch.ensure.on }
+  run { Side_Yard_Light_Power.ensure.command(ON) }
 
   only_if { Sun_Status.state == "DOWN" }
 end
@@ -11,5 +13,9 @@ end
 rule "when the laundry room door closes" do
   changed Laundry_Room_Door_Contact, to: CLOSED
 
-  run { Side_Yard_Lights_Switch.ensure.off }
+  run { Side_Yard_Light_Power.ensure.command(OFF) }
 end
+
+updated(Side_Yard_Lights_Scene_Number_Top, to: ZWave::PADDLE_CLICK) { Side_Yard_Light_Power.ensure.command(ON) }
+
+updated(Side_Yard_Lights_Scene_Number_Bottom, to: ZWave::PADDLE_CLICK) { Side_Yard_Light_Power.ensure.command(OFF) }
