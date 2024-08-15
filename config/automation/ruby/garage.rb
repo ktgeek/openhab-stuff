a94f9451fdaf5be1f688be68699f725e1b80d5d7
Keith T. Garner <kgarner@kgarner.com>
2024-08-15 18:36:21 -0500

    rewrite garage to use a hash over a case

--- config/automation/ruby/garage.rb
+++ config/automation/ruby/garage.rb
@@ -2,39 +2,35 @@
 
 require "homeseer"
 
+GARAGE_STATE_INFO = {
+  "OPENING" => { target_state: ON, blink_state: ON, color: Homeseer::LedColor::RED },
+  "OPEN" => { target_state: ON, blink_state: OFF, color: Homeseer::LedColor::RED },
+  "CLOSING" => { target_state: OFF, blink_state: ON, color: Homeseer::LedColor::GREEN },
+  "CLOSED" => { target_state: OFF, blink_state: OFF, color: Homeseer::LedColor::GREEN }
+}.freeze
+
 rule "garage doors state" do
   changed Small_Garage_Door_State, Large_Garage_Door_State
 
   run do |event|
-    case event.state
-    when "OPENING"
-      target_state = ON
-      blink_state = ON
-      color = Homeseer::LedColor::RED
-    when "OPEN"
-      target_state = ON
-      blink_state = OFF
-      color = Homeseer::LedColor::RED
-    when "CLOSING"
-      target_state = OFF
-      blink_state = ON
-      color = Homeseer::LedColor::GREEN
-    else # "CLOSED"
-      target_state = OFF
-      blink_state = OFF
-      color = Homeseer::LedColor::GREEN
-    end
+    state = event.state
+    info = GARAGE_STATE_INFO[state]
+
+    next unless info
 
     group = event.item.groups.first
-    items["#{group.name}_Target_State"].ensure.update(target_state)
 
-    TV_Notifications.command("#{group.label} #{event.state}")
+    ensure_states do
+      items["#{group.name}_Target_State"].update(info[:target_state])
 
-    blink_leds = items["#{group.name}_Open_LEDs_Blink"]
-    blink_leds.members.ensure.command(blink_state)
+      TV_Notifications.command("#{group.label} #{state}")
 
-    leds = items["#{group.name}_Open_LEDs"]
-    leds.members.ensure.command(color)
+      leds = items["#{group.name}_Open_LEDs"]
+      leds.members.command(info[:color])
+
+      blink_leds = items["#{group.name}_Open_LEDs_Blink"]
+      blink_leds.members.command(info[:blink_state])
+    end
   end
 end
 
