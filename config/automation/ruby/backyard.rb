865848e6e04edb76b8bbd57153174a78cc15cb13
Keith T. Garner <kgarner@kgarner.com>
2025-01-26 12:08:36 -0600

    move backyard lights to a trigger configuration

--- config/automation/ruby/backyard.rb
+++ config/automation/ruby/backyard.rb
@@ -2,10 +2,8 @@
 
 require "zwave"
 
-updated(Kitchen_Backyard_Lights_Scene_1, FamilyRoom_Backyard_Lights_Scene_1, to: ZWave::PADDLE_CLICK) do
-  Backyard_Lights_Power.ensure.on
-end
+BACKYARD_LIGHT_SWITCHES = [Kitchen_Backyard_Lights_Switch.thing, FamilyRoom_Backyard_Lights_Switch.thing].freeze
 
-updated(Kitchen_Backyard_Lights_Scene_2, FamilyRoom_Backyard_Lights_Scene_2, to: ZWave::PADDLE_CLICK) do
-  Backyard_Lights_Power.ensure.off
-end
+channel("scene_1", thing: BACKYARD_LIGHT_SWITCHES, triggered: ZWave::PADDLE_CLICK) { Backyard_Lights_Power.ensure.on }
+
+channel("scene_2", thing: BACKYARD_LIGHT_SWITCHES, triggered: ZWave::PADDLE_CLICK) { Backyard_Lights_Power.ensure.off }
