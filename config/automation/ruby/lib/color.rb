7e686f380f2f4976e3ae4f4f17c61e9e543c45dc
Keith T. Garner <kgarner@kgarner.com>
2025-03-10 12:59:57 -0500

    fix type-o

--- config/automation/ruby/lib/color.rb
+++ config/automation/ruby/lib/color.rb
@@ -4,7 +4,7 @@ logger.warn("Loading script #{__FILE__}")
 
 # Defining some colors I use a lot
 module Color
-  # color is a 360 value, then saturation and brighteness.  This is FULL RED
+  # color is a 360 value, then saturation and brightness.  This is FULL RED
   RED = OpenHAB::Core::Types::HSBType.new(0, 100, 100)
   WARM_WHITE = OpenHAB::Core::Types::HSBType.new(44, 13, 100)
   PURPLE = OpenHAB::Core::Types::HSBType.new(300, 100, 100)
