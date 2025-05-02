8c7f6b6489c1a31556a9f0180f7dd9260a0a0a19
Keith T. Garner <kgarner@kgarner.com>
2025-05-02 09:53:59 -0500

    update temp change rules
    Change the rules to only fire off if the thermostat is in heating mode.  Add constants to the zwave library file, and flush out and add map files for later use.

--- config/automation/ruby/lib/zwave.rb
+++ config/automation/ruby/lib/zwave.rb
@@ -11,4 +11,38 @@ module ZWave
     FOUR_CLICKS = 5
     FIVE_CLICKS = 6
   end
+
+  module Thermostat
+    module Mode
+      OFF = 0
+      HEAT = 1
+      COOL = 2
+      AUTO = 3
+      AUX = 4
+      RESUME = 5
+      FAN_ONLY = 6
+      FURNACE = 7
+      DRY = 8
+      MOIST = 9
+      AUTO_CHANGEOVER = 10
+      ENERGY_HEAT = 11
+      ENERGY_COOL = 12
+      AWAY = 13
+      # 14 is undefined (No Z-Wave mode 0x0e)
+      FULL_POWER = 15
+    end
+
+    module State
+      IDLE = 0
+      HEATING = 1
+      COOLING = 2
+      FAN = 3
+      IDLE_4 = 4
+      IDLE_5 = 5
+      FAN_6 = 6
+      HEATING_7 = 7
+      HEATING_8 = 8
+      COOLING_9 = 9
+    end
+  end
 end
--- config/automation/ruby/thermostats.rb
+++ config/automation/ruby/thermostats.rb
@@ -1,21 +1,31 @@
 # frozen_string_literal: true
 
+require "zwave"
+
 ensure_states!
 
 # We don't know how long it takes to warm to temp like the nest did, so we're going to go for a half hour before we want
 # temp
-every :monday, :tuesday, :wednesday, :thursday, :friday, at: "6am" do
-  FF_Thermostat_Min_Set_Point.command(73)
+rule "weekday morning temp change" do
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "6am"
+  run { FF_Thermostat_Min_Set_Point.command(73) }
+  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
-every :monday, :tuesday, :wednesday, :thursday, :friday, at: "10pm" do
-  FF_Thermostat_Min_Set_Point.command(65)
+rule "weekday evening temp change" do
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "9:30pm"
+  run { FF_Thermostat_Min_Set_Point.command(65) }
+  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
-every :saturday, :sunday, at: "7:30am" do
-  FF_Thermostat_Min_Set_Point.command(73)
+rule "weekend morning temp change" do
+  every :saturday, :sunday, at: "7:00am"
+  run { FF_Thermostat_Min_Set_Point.command(73) }
+  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
-every :saturday, :sunday, at: "10:45pm" do
-  FF_Thermostat_Min_Set_Point.command(65)
+rule "weekend evening temp change" do
+  every :saturday, :sunday, at: "10:15pm"
+  run { FF_Thermostat_Min_Set_Point.command(65) }
+  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
