a5a3360c47bfccf9ca083d11d338f0926ed87e55
Keith T. Garner <kgarner@kgarner.com>
2025-05-08 14:49:08 -0500

    dry up thermostat settings

--- config/automation/ruby/thermostats.rb
+++ config/automation/ruby/thermostats.rb
@@ -4,7 +4,7 @@ require "zwave"
 
 ensure_states!
 
-TIMING = {
+TEMP_SETTINGS = {
   FF_Thermostat => {
     day_temp: 73,
     night_temp: 65,
@@ -19,56 +19,34 @@ TIMING = {
   }
 }.freeze
 
-# We don't know how long it takes to warm to temp like the nest did, so we're going to go for a half hour before we want
-# temp
-rule "weekday morning first floor temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(FF_Thermostat, :week, :day_start)
-  run { FF_Thermostat_Min_Set_Point.command(TIMING.dig(FF_Thermostat, :day_temp)) }
-  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
+TEMP_SETTINGS.each do |thermostat, tsettings|
+  name = thermostat.name
+  set_point = items["#{name}_Min_Set_Point"]
+  mode = items["#{name}_Mode"]
 
-rule "weekday evening first floor temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(FF_Thermostat, :week, :night_start)
-  run { FF_Thermostat_Min_Set_Point.command(TIMING.dig(FF_Thermostat, :night_temp)) }
-  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
+  rule "weekday morning #{name} temp change" do
+    every :monday, :tuesday, :wednesday, :thursday, :friday, at: tsettings.dig(:week, :day_start)
+    run { set_point.command(tsettings[:day_temp]) }
+    only_if { mode.state == ZWave::Thermostat::Mode::HEAT }
+  end
 
-rule "weekend morning first floor temp change" do
-  every :saturday, :sunday, at: TIMING.dig(FF_Thermostat, :weekend, :day_start)
-  run { FF_Thermostat_Min_Set_Point.command(TIMING.dig(FF_Thermostat, :day_temp)) }
-  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
+  rule "weekday evening #{name} temp change" do
+    every :monday, :tuesday, :wednesday, :thursday, :friday, at: tsettings.dig(:week, :night_start)
+    run { set_point.command(tsettings[:night_temp]) }
+    only_if { mode.state == ZWave::Thermostat::Mode::HEAT }
+  end
 
-rule "weekend evening first floor temp change" do
-  every :saturday, :sunday, at: TIMING.dig(FF_Thermostat, :weekend, :night_start)
-  run { FF_Thermostat_Min_Set_Point.command(TIMING.dig(FF_Thermostat, :night_temp)) }
-  only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
+  rule "weekend morning #{name} temp change" do
+    every :saturday, :sunday, at: tsettings.dig(:weekend, :day_start)
+    run { set_point.command(tsettings[:day_temp]) }
+    only_if { mode.state == ZWave::Thermostat::Mode::HEAT }
+  end
 
-# Adding rules for the upstairs thermostat
-
-rule "weekday morning upstairs temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(SF_Thermostat, :week, :day_start)
-  run { SF_Thermostat_Min_Set_Point.command(TIMING.dig(SF_Thermostat, :day_temp)) }
-  only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
-
-rule "weekday evening upstairs temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(SF_Thermostat, :week, :night_start)
-  run { SF_Thermostat_Min_Set_Point.command(TIMING.dig(SF_Thermostat, :night_temp)) }
-  only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
-
-rule "weekend morning upstairs temp change" do
-  every :saturday, :sunday, at: TIMING.dig(SF_Thermostat, :weekend, :day_start)
-  run { SF_Thermostat_Min_Set_Point.command(TIMING.dig(SF_Thermostat, :day_temp)) }
-  only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
-end
-
-rule "weekend evening upstairs temp change" do
-  every :saturday, :sunday, at: TIMING.dig(SF_Thermostat, :weekend, :night_start)
-  run { SF_Thermostat_Min_Set_Point.command(TIMING.dig(SF_Thermostat, :night_temp)) }
-  only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
+  rule "weekend evening #{name} temp change" do
+    every :saturday, :sunday, at: tsettings.dig(:weekend, :night_start)
+    run { set_point.command(tsettings[:night_temp]) }
+    only_if { mode.state == ZWave::Thermostat::Mode::HEAT }
+  end
 end
 
 changed(FF_Thermostat_Mode, SF_Thermostat_Mode, to: ZWave::Thermostat::Mode::HEAT) do |event|
@@ -76,12 +54,12 @@ changed(FF_Thermostat_Mode, SF_Thermostat_Mode, to: ZWave::Thermostat::Mode::HEA
   set_point = items["#{item.groups.first.name}_Min_Set_Point"]
 
   time = Time.now
-  times = TIMING.dig(item, (time.weekend? ? :weekend : :week))
+  times = TEMP_SETTINGS.dig(item, (time.weekend? ? :weekend : :week))
 
   case time
   when between(times[:day_start]..times[:night_start])
-    set_point.command(TIMING.dig(event.item, :day_temp))
+    set_point.command(TEMP_SETTINGS.dig(item, :day_temp))
   when between(times[:night_start]..times[:day_start])
-    set_point.command(TIMING.dig(event.item, :night_temp))
+    set_point.command(TEMP_SETTINGS.dig(item, :night_temp))
   end
 end
