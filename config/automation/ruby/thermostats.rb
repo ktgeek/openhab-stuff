eeee2ae8dda7325606067850b79944f2e2e3cee4
Keith T. Garner <kgarner@kgarner.com>
2025-05-08 12:31:42 -0500

    add jump to default temp on activation

--- config/automation/ruby/thermostats.rb
+++ config/automation/ruby/thermostats.rb
@@ -4,28 +4,39 @@ require "zwave"
 
 ensure_states!
 
+TIMING = {
+  FF_Thermostat_Mode => {
+    WEEK: { day_start: "6am", night_start: "9:30pm" },
+    WEEKEND: { day_start: "7:00am", night_start: "10:15pm" }
+  },
+  SF_Thermostat_Mode => {
+    WEEK: { day_start: "5:15am", night_start: "9:30pm" },
+    WEEKEND: { day_start: "7:15am", night_start: "10:15pm" }
+  }
+}.freeze
+
 # We don't know how long it takes to warm to temp like the nest did, so we're going to go for a half hour before we want
 # temp
 rule "weekday morning first floor temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "6am"
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(FF_Thermostat_Mode, :WEEK, :day_start)
   run { FF_Thermostat_Min_Set_Point.command(73) }
   only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekday evening first floor temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "9:30pm"
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(FF_Thermostat_Mode, :WEEK, :night_start)
   run { FF_Thermostat_Min_Set_Point.command(65) }
   only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekend morning first floor temp change" do
-  every :saturday, :sunday, at: "7:00am"
+  every :saturday, :sunday, at: TIMING.dig(FF_Thermostat_Mode, :WEEKEND, :day_start)
   run { FF_Thermostat_Min_Set_Point.command(73) }
   only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekend evening first floor temp change" do
-  every :saturday, :sunday, at: "10:15pm"
+  every :saturday, :sunday, at: TIMING.dig(FF_Thermostat_Mode, :WEEKEND, :night_start)
   run { FF_Thermostat_Min_Set_Point.command(65) }
   only_if { FF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
@@ -33,25 +44,37 @@ end
 # Adding rules for the upstairs thermostat
 
 rule "weekday morning upstairs temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "5:15am"
-  run { SF_Thermostat_Min_Set_Point.command(72) }
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(SF_Thermostat_Mode, :WEEK, :day_start)
+  run { SF_Thermostat_Min_Set_Point.command(73) }
   only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekday evening upstairs temp change" do
-  every :monday, :tuesday, :wednesday, :thursday, :friday, at: "9:30pm"
-  run { SF_Thermostat_Min_Set_Point.command(66) }
+  every :monday, :tuesday, :wednesday, :thursday, :friday, at: TIMING.dig(SF_Thermostat_Mode, :WEEK, :night_start)
+  run { SF_Thermostat_Min_Set_Point.command(68) }
   only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekend morning upstairs temp change" do
-  every :saturday, :sunday, at: "7:15am"
-  run { SF_Thermostat_Min_Set_Point.command(72) }
+  every :saturday, :sunday, at: TIMING.dig(SF_Thermostat_Mode, :WEEKEND, :day_start)
+  run { SF_Thermostat_Min_Set_Point.command(73) }
   only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
 end
 
 rule "weekend evening upstairs temp change" do
-  every :saturday, :sunday, at: "10:15pm"
-  run { SF_Thermostat_Min_Set_Point.command(66) }
+  every :saturday, :sunday, at: TIMING.dig(SF_Thermostat_Mode, :WEEKEND, :night_start)
+  run { SF_Thermostat_Min_Set_Point.command(68) }
   only_if { SF_Thermostat_Mode.state == ZWave::Thermostat::Mode::HEAT }
+end
+
+changed(FF_Thermostat_Mode, SF_Thermostat_Mode, to: ZWave::Thermostat::Mode::HEAT) do |event|
+  time = Time.now
+  times = TIMING.dig(event.item, (time.weekend? ? :WEEKEND : :WEEK))
+
+  case time
+  when between(times[:day_start]..times[:night_start])
+    FF_Thermostat_Min_Set_Point.command(73)
+  when between(times[:night_start]..times[:day_start])
+    FF_Thermostat_Min_Set_Point.command(65)
+  end
 end
