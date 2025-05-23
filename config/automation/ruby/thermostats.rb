0068b56333a6fb482d58d70cf0a07be39f33511b
Keith T. Garner <kgarner@kgarner.com>
2025-05-23 08:11:55 -0500

    rework the thermostat hash
    Removed some repetition

--- config/automation/ruby/thermostats.rb
+++ config/automation/ruby/thermostats.rb
@@ -7,70 +7,66 @@ ensure_states!
 
 TEMP_SETTINGS = {
   FF_Thermostat => {
-    temps: { ZWave::Thermostat::Mode::HEAT => { day: 73, night: 65 } },
-    schedule: [
-      {
-        name: "sunday",
-        days: %i[sunday],
-        day_start: "7:15am",
-        night_start: "9:30pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "weekday",
-        days: %i[monday tuesday wednesday thursday],
-        day_start: "6am",
-        night_start: "9:30pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "friday",
-        days: %i[friday],
-        day_start: "6am",
-        night_start: "10:15pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "saturday",
-        days: %i[saturday],
-        day_start: "7:15am",
-        night_start: "10:15pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      }
-    ]
+    ZWave::Thermostat::Mode::HEAT => {
+      temps: { day: 73, night: 65 },
+      schedule: [
+        {
+          name: "sunday",
+          days: %i[sunday],
+          day_start: "7:15am",
+          night_start: "9:30pm"
+        },
+        {
+          name: "weekday",
+          days: %i[monday tuesday wednesday thursday],
+          day_start: "6am",
+          night_start: "9:30pm"
+        },
+        {
+          name: "friday",
+          days: %i[friday],
+          day_start: "6am",
+          night_start: "10:15pm"
+        },
+        {
+          name: "saturday",
+          days: %i[saturday],
+          day_start: "7:15am",
+          night_start: "10:15pm"
+        }
+      ]
+    }
   },
   SF_Thermostat => {
-    temps: { ZWave::Thermostat::Mode::HEAT => { day: 73, night: 68 } },
-    schedule: [
-      {
-        name: "sunday",
-        days: %i[sunday],
-        day_start: "7:00am",
-        night_start: "9:30pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "weekday",
-        days: %i[monday tuesday wednesday thursday],
-        day_start: "5:15am",
-        night_start: "9:30pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "friday",
-        days: %i[friday],
-        day_start: "5:15am",
-        night_start: "10:15pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      },
-      {
-        name: "saturday",
-        days: %i[saturday],
-        day_start: "7:00am",
-        night_start: "10:15pm",
-        mode: ZWave::Thermostat::Mode::HEAT
-      }
-    ]
+    ZWave::Thermostat::Mode::HEAT => {
+      temps: { day: 73, night: 68 },
+      schedule: [
+        {
+          name: "sunday",
+          days: %i[sunday],
+          day_start: "7:00am",
+          night_start: "9:30pm"
+        },
+        {
+          name: "weekday",
+          days: %i[monday tuesday wednesday thursday],
+          day_start: "5:15am",
+          night_start: "9:30pm"
+        },
+        {
+          name: "friday",
+          days: %i[friday],
+          day_start: "5:15am",
+          night_start: "10:15pm"
+        },
+        {
+          name: "saturday",
+          days: %i[saturday],
+          day_start: "7:00am",
+          night_start: "10:15pm"
+        }
+      ]
+    }
   }
 }.freeze
 
@@ -79,19 +75,21 @@ TEMP_SETTINGS.each do |thermostat, tsettings|
   set_point = items["#{thermostat_name}_Min_Set_Point"]
   thermostat_mode = items["#{thermostat_name}_Mode"]
 
-  tsettings[:schedule].each do |schedule|
-    temps = tsettings.dig(:temps, schedule[:mode])
+  tsettings.each do |mode, mode_settings|
+    mode_settings[:schedule].each do |schedule|
+      temps = mode_settings[:temps]
 
-    rule "#{thermostat_name} #{schedule[:name]} day temp change" do
-      every(*schedule[:days], at: schedule[:day_start])
-      run { set_point.command(temps[:day]) }
-      only_if { thermostat_mode.state.to_i == schedule[:mode] }
-    end
+      rule "#{thermostat_name} #{schedule[:name]} #{mode} day temp change" do
+        every(*schedule[:days], at: schedule[:day_start])
+        run { set_point.command(temps[:day]) }
+        only_if { thermostat_mode.state.to_i == mode }
+      end
 
-    rule "#{thermostat_name} #{schedule[:name]} night temp change" do
-      every(*schedule[:days], at: schedule[:night_start])
-      run { set_point.command(temps[:night]) }
-      only_if { thermostat_mode.state.to_i == schedule[:mode] }
+      rule "#{thermostat_name} #{schedule[:name]} #{mode} night temp change" do
+        every(*schedule[:days], at: schedule[:night_start])
+        run { set_point.command(temps[:night]) }
+        only_if { thermostat_mode.state.to_i == mode }
+      end
     end
   end
 end
@@ -100,16 +98,16 @@ changed(FF_Thermostat_Mode, SF_Thermostat_Mode) do |event|
   next if event.state.to_i == ZWave::Thermostat::Mode::OFF
 
   thermostat = event.item.groups.first
-  tsetting = TEMP_SETTINGS[thermostat]
   mode = event.state.to_i
+  mode_setting = TEMP_SETTINGS.dig(thermostat, mode)
 
   time = Time.now
-  schedule = tsetting[:schedule].find { |s| s[:mode] == mode && s[:days].include?(TimeHelpers::DAY_OF_WEEK[time.wday]) }
+  schedule = mode_setting[:schedule].find { |s| s[:days].include?(TimeHelpers::DAY_OF_WEEK[time.wday]) }
 
   next unless schedule
 
   set_point = items["#{thermostat.name}_Min_Set_Point"]
-  temps = tsetting.dig(:temps, mode)
+  temps = mode_setting[:temps]
 
   case time
   when between(schedule[:day_start]..schedule[:night_start])
