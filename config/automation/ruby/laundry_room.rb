e5d840e621e37472a653671328c2b1fb07d265bf
Keith T. Garner <kgarner@kgarner.com>
2026-05-13 10:03:21 -0500

    fix laundry room contact

--- config/automation/ruby/laundry_room.rb
+++ config/automation/ruby/laundry_room.rb
@@ -3,14 +3,14 @@
 require "zwave"
 
 rule "when the laundry room door opens" do
-  changed Laundry_Room_Door_Contact, to: OPEN
+  changed Laundry_Room_Door_Contact, to: ON
 
   run { Side_Yard_Light_Color.ensure.on }
 
   only_if { Sun_Status.state == "DOWN" }
 end
 
-changed(Laundry_Room_Door_Contact, to: CLOSED) { Side_Yard_Light_Color.ensure.off }
+changed(Laundry_Room_Door_Contact, to: OFF) { Side_Yard_Light_Color.ensure.off }
 
 updated(Side_Yard_Lights_Scene_1, to: ZWave::Paddle::CLICK) { Side_Yard_Light_Color.ensure.on }
 
