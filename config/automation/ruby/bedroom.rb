b021b70d0184ecc587b041ed6418f24694136bc9
Keith T. Garner <kgarner@kgarner.com>
2026-06-21 07:40:35 -0500

    move bedroom ceiling fan to zigbee

--- config/automation/ruby/bedroom.rb
+++ config/automation/ruby/bedroom.rb
@@ -5,20 +5,14 @@ require "zigbee"
 
 BEDROOM_REMOTES = [Bedroom_Table_Remote_Battery.thing, Bedroom_Sarah_Remote_Battery.thing].freeze
 
-updated Bedroom_Ceiling_Fan_Speed do |event|
-  next unless (0..3).cover?(event.state)
-
-  Bedroom_Ceiling_Fan_Power.update(event.state.positive?)
-end
-
-received_command(Bedroom_Ceiling_Fan_Power, command: OFF) { Bedroom_Ceiling_Fan_Speed.command(0) }
-
 channel(%w[1_single 2_single 3_single], thing: BEDROOM_REMOTES) do |event|
-  Bedroom_Ceiling_Fan_Speed.command(event.event[0].to_i)
+  speed = 100 * (event.event[0].to_f / 3)
+
+  Bedroom_Ceiling_Fan_Speed.ensure.command(speed)
 end
 
 channel(%w[1_double 2_double 3_double], thing: BEDROOM_REMOTES) do
-  Bedroom_Ceiling_Fan_Speed.command(0)
+  Bedroom_Ceiling_Fan_Speed.ensure.command(0)
 end
 
 channel("4_single", thing: BEDROOM_REMOTES) do |event|
@@ -26,7 +20,7 @@ channel("4_single", thing: BEDROOM_REMOTES) do |event|
   switch&.toggle
 end
 
-channel("4_double", thing: BEDROOM_REMOTES) { Bedroom_Ceiling_Fan_Light_Power.toggle }
+channel("4_double", thing: BEDROOM_REMOTES) { Bedroom_Ceiling_Fan_Light.toggle }
 
 channel("4_hold", thing: BEDROOM_REMOTES.first) { Bedroom_Sarah_Light_Switch.toggle }
 
@@ -49,13 +43,13 @@ updated(Bedroom_Hallway_Light_Scene_1, to: ZWave::Paddle::CLICK) { Bedroom_Table
 updated(Bedroom_Hallway_Light_Scene_2, to: ZWave::Paddle::CLICK) { Bedroom_Sarah_Light_Switch.toggle }
 
 updated(Bedroom_Hallway_Light_Scene_3, to: ZWave::Paddle::CLICK) do
-  if Bedroom_Ceiling_Fan_Power.on?
+  if Bedroom_Ceiling_Fan_Speed.on?
     Bedroom_Ceiling_Fan_Speed.command(0)
   else
-    Bedroom_Ceiling_Fan_Speed.command(3)
+    Bedroom_Ceiling_Fan_Speed.command(100)
   end
 end
 
 updated(Bedroom_Hallway_Light_Scene_4, to: ZWave::Paddle::CLICK) { Bedroom_Hallway_Light_Switch.toggle }
 
-updated(Bedroom_Hallway_Light_Scene_5, to: ZWave::Paddle::CLICK) { Bedroom_Ceiling_Fan_Light_Power.toggle }
+updated(Bedroom_Hallway_Light_Scene_5, to: ZWave::Paddle::CLICK) { Bedroom_Ceiling_Fan_Light.toggle }
