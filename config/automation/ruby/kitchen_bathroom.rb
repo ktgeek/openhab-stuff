1cab08f2f3ffd38432e187e0d09400f47d67b000
Keith T. Garner <kgarner@kgarner.com>
2026-06-24 14:19:26 -0500

    kitchen bathroom naming scheme updates

--- config/automation/ruby/kitchen_bathroom.rb
+++ config/automation/ruby/kitchen_bathroom.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-changed(Kitchen_Bathroom_Contact_Sensor, to: ON) do
-  timers.cancel(Kitchen_Bathroom_Light_Switch)
-  timers.cancel(Kitchen_Bathroom_Fan_Switch)
+changed(KitchenBathroom_ContactSensor, to: ON) do
+  timers.cancel(KitchenBathroom_Light_Switch)
+  timers.cancel(KitchenBathroom_Fan_Switch)
 
-  after(2.minutes, id: Kitchen_Bathroom_Light_Switch) { Kitchen_Bathroom_Light_Switch.ensure.off }
-  after(5.minutes, id: Kitchen_Bathroom_Fan_Switch) { Kitchen_Bathroom_Fan_Switch.ensure.off }
+  after(2.minutes, id: KitchenBathroom_Light_Switch) { KitchenBathroom_Light_Switch.ensure.off }
+  after(5.minutes, id: KitchenBathroom_Fan_Switch) { KitchenBathroom_Fan_Switch.ensure.off }
 end
 
-changed(Kitchen_Bathroom_Contact_Sensor, to: OFF) do
-  timers.cancel(Kitchen_Bathroom_Light_Switch)
-  timers.cancel(Kitchen_Bathroom_Fan_Switch)
+changed(KitchenBathroom_ContactSensor, to: OFF) do
+  timers.cancel(KitchenBathroom_Light_Switch)
+  timers.cancel(KitchenBathroom_Fan_Switch)
 end
