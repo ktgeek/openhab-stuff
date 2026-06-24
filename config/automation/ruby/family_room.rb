5b127dd501fb0318863988ef80b12984c945054e
Keith T. Garner <kgarner@kgarner.com>
2026-06-24 10:33:51 -0500

    family room naming scheme updates

--- config/automation/ruby/family_room.rb
+++ config/automation/ruby/family_room.rb
@@ -8,8 +8,8 @@ rule "when family room lights turn on/off, turn on/off LEDs" do
   run { |event| FamilyRoom_LED_Power.ensure.command(event.state) }
 end
 
-channel(Zigbee::SINGLE_TAP, thing: Family_Room_Fan_Button_Battery.thing) { FamilyRoom_Lights_Switch.toggle }
+channel(Zigbee::SINGLE_TAP, thing: FamilyRoom_Fan_Button_Battery.thing) { FamilyRoom_Lights_Switch.toggle }
 
-channel(Zigbee::DOUBLE_TAP, thing: Family_Room_Fan_Button_Battery.thing) { Family_Room_Ceiling_Fan_Light_Power.toggle }
+channel(Zigbee::DOUBLE_TAP, thing: FamilyRoom_Fan_Button_Battery.thing) { Family_Room_Ceiling_Fan_Light_Power.toggle }
 
-channel(Zigbee::HOLD, thing: Family_Room_Fan_Button_Battery.thing) { Family_Room_Ceiling_Fan_Fan_Power.toggle }
+channel(Zigbee::HOLD, thing: FamilyRoom_Fan_Button_Battery.thing) { Family_Room_Ceiling_Fan_Fan_Power.toggle }
