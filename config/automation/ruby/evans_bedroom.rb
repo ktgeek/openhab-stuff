ae667d748a7d6718b615d9ab23c047fb0e85014d
Keith T. Garner <kgarner@kgarner.com>
2026-07-15 16:24:09 -0500

    lint fix

--- config/automation/ruby/evans_bedroom.rb
+++ config/automation/ruby/evans_bedroom.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "zigbee"
 
 channel(Zigbee::SINGLE_TAP, thing: "homeassistant:device:26bcbec1ee:zigbee2mqtt_5F0x282c02bfffee018b") { EvansRoom_LED_Power.toggle }
