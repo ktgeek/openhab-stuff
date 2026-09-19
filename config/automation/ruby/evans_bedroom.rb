b7728d0bfa7880c980fd5bde1a7ffc8db64a783c
Keith T. Garner <kgarner@kgarner.com>
2026-09-19 16:47:30 -0500

    move evan bedroom leds to color only and matter

--- config/automation/ruby/evans_bedroom.rb
+++ config/automation/ruby/evans_bedroom.rb
@@ -2,4 +2,6 @@
 
 require "zigbee"
 
-channel(Zigbee::SINGLE_TAP, thing: "homeassistant:device:26bcbec1ee:zigbee2mqtt_5F0x282c02bfffee018b") { EvansRoom_LED_Power.toggle }
+channel(Zigbee::SINGLE_TAP, thing: "homeassistant:device:26bcbec1ee:zigbee2mqtt_5F0x282c02bfffee018b") do
+  EvansRoom_LED_Color.toggle
+end
